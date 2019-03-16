4f1ba2d522d6fcc7332e6aa8ec1ba1fb3682ee10
Kevin Schaefer <keschae@ilstu.edu>
2019-03-16 14:37:30 -0500

    added card-keschae

--- src/app/card-keschae/card-keschae.component.ts
+++ src/app/card-keschae/card-keschae.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Student } from '../student';
+
 @Component({
   selector: 'app-card-keschae',
   templateUrl: './card-keschae.component.html',
@@ -7,7 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardKeschaeComponent implements OnInit {
 
-  constructor() { }
+  me: Student;
+
+  constructor() {
+    this.me = {
+      ulid: 'keschae',
+      name: 'Kevin Schaefer',
+      desc_short: 'Small business manager, web developer and university instructor',
+      desc_long: 'Small business manager, web developer and university instructor. I never stop learning. \
+      Always learning something new. I used to love playing music but I will be always a fan. \
+      Love the band Umphrey\'s McGee and many other popular and eclectic musicians.',
+      major: 'Applied Computer Science',
+      platform: 'JavaScript: Angular and PHP: Laravel',
+      hometown: 'Ellsworth, Illinois, USA',
+      hobbies: 'Guitars, music, coding',
+      inspiration: 'Teddy Roosevelt',
+      website: 'https://www.sitepoint.com',
+      school: 'Illinois Wesleyan University'
+    };
+  }
 
   ngOnInit() {
   }
