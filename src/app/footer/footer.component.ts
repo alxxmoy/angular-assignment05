399d210cb61c5a25ac86c2f0057922d2bbe150eb
Kevin Schaefer <keschae@ilstu.edu>
2019-03-16 14:32:16 -0500

    added footer and nav components

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -2,8 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  // templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css'],
+  template: `
+  <footer>
+  <div class="container">
+      <p class="float-right"><a href="#">Back to top</a></p>
+      <p>Created by Kevin Schaefer for IT354.</p>
+      <p>&copy; 2018 Illinois State University</p>
+  </div>
+  </footer>
+  `
 })
 export class FooterComponent implements OnInit {
 
